ed7fc0f0c0f30e49d8a79eae2f948160a97e109c
ChenHaoHSU <b03505028@ntu.edu.tw>
2018-10-24 02:27:57 +0800

    upd pa2 run.sh

--- pa2/run.sh
+++ pa2/run.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-./atpg ../sample_circuits/c$1.ckt > ../reports/c$1.report
-../bin/golden_atpg ../sample_circuits/c$1.ckt > ../reports/golden2_c$1.report
+EXE=./atpg
+GOLDEN=../bin/golden_atpg
+CIRPATH=../sample_circuits
+RPTPATH=../reports
 
-diff ../reports/c$1.report ../reports/golden2_c$1.report
+ALLCASES="17 432 499 880 1355 1908 2670 3540 5315 6288 7552"
 
+if [ $# -eq 0 ]
+then
+    CASES=$ALLCASES
+else
+    CASES=$1
+fi
 
+for case in $CASES
+do
+  CKT_FILE=c$case.ckt
+  RPT=c$case.report
+  GOLDEN_RPT=golden2_c$case.report
+  echo "##### $CKT_FILE BEGIN #####"
+  $EXE $CIRPATH/$CKT_FILE > $RPTPATH/$RPT
+  $GOLDEN $CIRPATH/$CKT_FILE > $RPTPATH/$GOLDEN_RPT
+  diff $RPTPATH/$RPT $RPTPATH/$GOLDEN_RPT
+  echo "##### $CKT_FILE END #######"
+  echo ""
+  echo "---------------------------------------------------------"
+  echo ""
+done
