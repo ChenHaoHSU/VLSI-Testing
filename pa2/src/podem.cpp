280c51c04764dcde3cd88432c7fca5087eb6d5c2
ChenHaoHSU <b03505028@ntu.edu.tw>
2018-10-25 00:15:53 +0800

    upd

--- pa2/src/podem.cpp
+++ pa2/src/podem.cpp
@@ -358,8 +358,8 @@ ATPG::wptr ATPG::find_pi_assignment(const wptr object_wire, const int& object_le
 		// TODO find the input wire  
     // Hint similar to OR and NAND but different polarity
     //--------------------------------- hole -------------------------------------------
-        if (object_level) new_object_wire = find_hardest_control(object_wire->inode.front());  // decision gate
-        else new_object_wire = find_easiest_control(object_wire->inode.front()); // imply gate
+        if (object_level) new_object_wire = find_hardest_control(object_wire->inode.front());
+        else new_object_wire = find_easiest_control(object_wire->inode.front());
         break;
     //----------------------------------------------------------------------------------
 		//  TODO 
