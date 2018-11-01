4af63032d784aa58331252163c310e6f8d1baa4c
ChenHaoHSU <b03505028@ntu.edu.tw>
2018-11-01 11:34:41 +0800

    pa3 upload

---
+++ pa3/src/tdfsim.cpp
@@ -0,0 +1,16 @@
+/**********************************************************************/
+/*  Parallel-Fault Event-Driven Transition Delay Fault Simulator      */
+/*                                                                    */
+/**********************************************************************/
+
+#include "atpg.h"
+
+/* pack 16 faults into one packet.  simulate 16 faults togeter. 
+ * the following variable name is somewhat misleading */
+#define num_of_pattern 16
+
+
+void ATPG::transition_delay_fault_simulation() {
+  
+}
+
